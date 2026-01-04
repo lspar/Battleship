7056f90efe48d3028dad4f3c952c8d3589d11492
Madeleine Lorentz <lorentz@udel.edu>
2026-01-04 16:11:56 -0500

    Made 8x8 grid

--- game.py
+++ game.py
@@ -1,1 +1,15 @@
+## Mak3 an 8 x 8 grid filled with "~" and print it 
+'''
+Step 1) Define main(), make a list with 8 squiggles and print it 
+'''
+
+
+
+def main():
+    grid=[]
+    squiggles=["~", "~", "~", "~", "~", "~", "~", "~"]
+    for squiggle in range(8):
+        grid.append(squiggles)
+    print(grid)
+main()
 

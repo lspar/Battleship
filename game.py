3b2c3bed7765156b9962bf49541a8fd7bfa56d19
Lindsey Spar <lspar@udel.edu>
2026-01-06 16:21:42 -0500

    shoot function

--- game.py
+++ game.py
@@ -5,11 +5,23 @@ Step 1) Define main(), make a list with 8 squiggles and print it
 
 import copy
 
+def shoot(ships: set, grid: list, row: int, col:int):
+    if grid[row][col] !="X" and grid[row][col]!="0":
+        if (row, col) in ships:
+            grid[row][col] = "X"
+        else:
+            grid[row][col] = "0"
+
+
 def main():
-    grid=[]
+    grid = []
+    ships = {(1,2), (1,3)}
     squiggles=["~", "~", "~", "~", "~", "~", "~", "~"]
     for squiggle in range(8):
         grid.append(copy.deepcopy(squiggles))
-    grid[1][2]="X"
+    #grid[1][2]="X"
+    shoot(ships, grid, 1,0)
     print(grid)
+
+
 main()

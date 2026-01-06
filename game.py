7c7a3eb5443ded61e1bd809e2ee26e7cca92bc1b
Lindsey Spar <lspar@udel.edu>
2026-01-06 14:36:00 -0500

    added the X

--- game.py
+++ game.py
@@ -3,13 +3,13 @@
 Step 1) Define main(), make a list with 8 squiggles and print it 
 '''
 
-
+import copy
 
 def main():
     grid=[]
     squiggles=["~", "~", "~", "~", "~", "~", "~", "~"]
     for squiggle in range(8):
-        grid.append(squiggles)
+        grid.append(copy.deepcopy(squiggles))
+    grid[1][2]="X"
     print(grid)
 main()
-

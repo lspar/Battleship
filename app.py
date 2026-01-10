2b1c5070a7d483dcb85864d4a763491b0a359b65
Lindsey Spar <lspar@udel.edu>
2026-01-10 14:29:00 -0500

    row and col selectbox

--- app.py
+++ app.py
@@ -1,23 +1,30 @@
 import streamlit as st
 import pandas as pd 
+from game import shoot
 import copy
-st.title("Batttleship")
 
-grid = []
-squiggles=["~", "~", "~", "~", "~", "~", "~", "~"]
-for squiggle in range(8):
-        grid.append(copy.deepcopy(squiggles))
+st.title("Battleship")
+
+def make_grid():
+        grid= []
+        squiggles=["~", "~", "~", "~", "~", "~", "~", "~"]
+        for squiggle in range(8):
+                grid.append(copy.deepcopy(squiggles))
 #for row in grid:
         #st.write(" ".join(row))
-
-df = pd.DataFrame(grid) #creates a table with the grid we made
-st.table(df)
+        return grid
 
 if "grid" not in st.session_state:
-        st.session_state.grid = []
+        st.session_state.grid = make_grid()
 
 if "ships" not in st.session_state:
-        st.session_state.ships = []
+        st.session_state.ships = {(1,2), (1,3)}
 
 if "hits" not in st.session_state:
         st.session_state.hits = set()
+
+df = pd.DataFrame(st.session_state.grid) #creates a table with the grid we made
+st.table(df)
+
+st.selectbox("Row",(0,1,2,3,4,5,6,7))
+st.selectbox("Column", (0,1,2,3,4,5,6,7))

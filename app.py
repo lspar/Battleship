38b50ce0c9ce8e796b65d73ffe71cab77ef89eff
Lindsey Spar <lspar@udel.edu>
2026-01-10 13:50:08 -0500

    initialize session_state

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd 
 import copy
 st.title("Batttleship")
 
@@ -6,5 +7,17 @@ grid = []
 squiggles=["~", "~", "~", "~", "~", "~", "~", "~"]
 for squiggle in range(8):
         grid.append(copy.deepcopy(squiggles))
-for row in grid:
-        st.write(" ".join(row))
+#for row in grid:
+        #st.write(" ".join(row))
+
+df = pd.DataFrame(grid) #creates a table with the grid we made
+st.table(df)
+
+if "grid" not in st.session_state:
+        st.session_state.grid = []
+
+if "ships" not in st.session_state:
+        st.session_state.ships = []
+
+if "hits" not in st.session_state:
+        st.session_state.hits = set()

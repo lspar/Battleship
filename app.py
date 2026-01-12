5dbedab98ae73fad9364fed299e0360e264b752f
Madeleine Lorentz <lorentz@udel.edu>
2026-01-12 14:55:27 -0500

    Edited button conditions

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ import copy
 
 st.title("Battleship!")
 
+
+
+
 def make_grid():
         grid= []
         squiggles=["~", "~", "~", "~", "~", "~", "~", "~"]
@@ -13,7 +16,6 @@ def make_grid():
 #for row in grid:
         #st.write(" ".join(row))
         return grid
-
 if "grid" not in st.session_state:
         st.session_state.grid = make_grid()
 
@@ -26,14 +28,35 @@ if "hits" not in st.session_state:
 if "message" not in st.session_state:
         st.session_state.message = ""
 
+if "button_clicked" not in st.session_state:
+        st.session_state.button_clicked=False
+
+def reset_game():
+        st.session_state.grid=make_grid()
+        st.session_state.ships = {(1,2), (1,3)}
+        st.session_state.hits = set()
+        st.session_state.message = ""
+        st.session_state.button_clicked=False
+if st.button("New Game"):
+        reset_game()
+
+def hide_button():
+        st.session_state.button_clicked=True
+
+
+
 
 row = st.selectbox("Row",(0,1,2,3,4,5,6,7))
 col = st.selectbox("Column", (0,1,2,3,4,5,6,7))
-if st.button("Fire!"):
-        st.session_state.message = shoot(st.session_state.ships, st.session_state.grid, st.session_state.hits, row, col)
-        st.info(st.session_state.message)
-        if st.session_state.hits == st.session_state.ships:
-                st.info("You Win!")
+
+if not st.session_state.button_clicked:
+        if st.button("Fire!"):
+                st.session_state.message = shoot(st.session_state.ships, st.session_state.grid, st.session_state.hits, row, col)
+                st.info(st.session_state.message)
+                if st.session_state.hits == st.session_state.ships:
+                        st.success("You Win!")
+                        hide_button()
+
 
 df = pd.DataFrame(st.session_state.grid) #creates a table with the grid we made
 st.table(df)
